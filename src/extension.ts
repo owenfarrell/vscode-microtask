26c51db239351b7cf9dd73c20f04d09451148bf5
Owen Farrell <owen.farrell@gmail.com>
2020-04-23 13:48:16 -0400

    Simplified iteration

--- src/extension.ts
+++ src/extension.ts
@@ -4,26 +4,29 @@ import * as fs from "fs";
 import * as Path from "path";
 import * as vscode from "vscode";
 
-const outputChannel: vscode.OutputChannel = vscode.window.createOutputChannel("Sub Launcher");
+const outputChannel = vscode.window.createOutputChannel("Sub Launcher");
 let taskItems: TaskQuickPickItem[] = [];
-let cwdMap: { [key: string]: string; } = {};
+let cwdMap = new Map<string, string>();
 
 export function activate(context: vscode.ExtensionContext) {
     // If a workspace has been opened
     if (vscode.workspace.rootPath) {
-        // When the configuration changes, reload the task list
-        vscode.workspace.onDidChangeConfiguration(loadTaskList);
         // Load the task list for the first time
         loadTaskList();
-        // Subscribe to "microtask.runFromHere" events
-        context.subscriptions.push(vscode.commands.registerCommand("microtask.runFromHere", runFromHere));
-        // Subscribe to "task process end" events
-        vscode.tasks.onDidEndTaskProcess(resetTaskCwd);
+
+        context.subscriptions.push(
+            // Subscribe to "change configuration" events
+            vscode.workspace.onDidChangeConfiguration(loadTaskList),
+            // Register the command
+            vscode.commands.registerCommand("microtask.runFromHere", runFromHere),
+            // Subscribe to "task process end" events
+            vscode.tasks.onDidEndTaskProcess(resetTaskCwd),
+            outputChannel
+        );
     }
 }
 
 export function deactivate() {
-    outputChannel.dispose();
 }
 
 interface TaskQuickPickItem extends vscode.QuickPickItem {
@@ -33,13 +36,21 @@ interface TaskQuickPickItem extends vscode.QuickPickItem {
 function loadTaskList() {
     // Fetch the list of tasks
     vscode.tasks.fetchTasks()
-        // Wrap each task in a QuickPickItem
-        .then((taskList: vscode.Task[]) => {
-            let refreshedList = taskList.map((element: vscode.Task) => <TaskQuickPickItem>{ description: "", label: element.name, task: element });
-            let refreshedMap = {};
-            taskItems.forEach((element: TaskQuickPickItem) => refreshedMap[element.task.name] = element.task.execution.options.cwd)
+        .then((taskList) => {
+            // Create an array of QuickPickItems
+            let refreshedList: TaskQuickPickItem[] = [];
+            // Create a Map of
+            let refreshedMap = new Map<string, string>();
+            // For each task
+            taskList.forEach((element) => {
+                // Wrap the task in a QuickPickItem and push it on to the array
+                refreshedList.push({ description: "", label: element.name, task: element });
+                // Cache the default CWD in the map
+                refreshedMap[element.name] = element.execution.options.cwd
+            });
             // Replace the cached list of QuickPickItems
             taskItems = refreshedList;
+            // Replace the cached map of Task CWDs
             cwdMap = refreshedMap;
         });
 }
@@ -48,7 +59,7 @@ function runFromHere(uri: vscode.Uri) {
     // Show the quick pick menu
     vscode.window.showQuickPick<TaskQuickPickItem>(taskItems)
         // Spawn a process based on the selected task
-        .then((selectedItem: TaskQuickPickItem) => {
+        .then((selectedItem) => {
             if (selectedItem) {
                 // Override the CWD for the task
                 selectedItem.task.execution.options.cwd = uri ? fs.lstatSync(uri.fsPath).isDirectory() ? uri.fsPath : Path.dirname(uri.fsPath) : vscode.workspace.rootPath;
@@ -59,6 +70,7 @@ function runFromHere(uri: vscode.Uri) {
 }
 
 function resetTaskCwd(event: vscode.TaskProcessEndEvent) {
+    // TODO Possible race condition where configuration is reloaded after task execution started
     // Replace the CWD with the original value
-    event.execution.task.execution.options.cwd = cwdMap[event.execution.task.name];
+    event.execution.task.execution.options.cwd = cwdMap.get(event.execution.task.name);
 }
